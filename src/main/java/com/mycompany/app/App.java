d19028bc6ffa697ad3a301088b61dfced339349d
Anumeghana <35059002+Anumeghana@users.noreply.github.com>
2019-04-06 16:38:09 +0530

    added more names

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello AnuBhaskar";
+    private final String message = "Hello AnuBhaskar-vashist,vyas";
 
     public App() {}
 
